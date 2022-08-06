985e2910983e0e7adf4e6a4a002ed0ab38979c8c
EsAlco <90135660+EsAlco@users.noreply.github.com>
2022-08-06 15:45:13 +0200

    Create project.

---
+++ FirebaseChat/FirebaseChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseChat
+//
+//  Created by Esther Alcoceba Gutiérrez de León on 6/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
